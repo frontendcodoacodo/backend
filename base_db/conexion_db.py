2601d9406bf6194e1166781accb06954a5e509b4
juampa08 <jpbenitez08@gmail.com>
2024-07-02 00:08:16 -0300

    creacion base, misma forma ejemplo en clase.

---
+++ base_db/conexion_db.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+#config de desarrollo local, establezco la conexion
+config_dev = {
+  'user': 'root',
+  'password': '',
+  'host': '127.0.0.1',
+  'database': 'turismo_db1',
+}
+
+#datos de nuestro despliegue: pythonanywhere, editar cuando tengamos
+config_prod = {
+  'user': 'ChToleCAC',
+  'password': '2024CACPythonFS',
+  'host': 'ChToleCAC.mysql.pythonanywhere-services.com',
+  'database': 'ChToleCAC$default',
+}
+
+conexion = mysql.connector.connect(**config_dev)  
