cab8ebb5958beca8546d4c857de4b9de5ae83104
juampa08 <jpbenitez08@gmail.com>
2024-07-03 14:18:58 -0300

    agrego modelos.py y tabla_db.py

--- componentes/consultar_db.py
+++ componentes/consultar_db.py
@@ -3,7 +3,7 @@ import base_db.conexion_db as conexion
 def traer_todos():
     con = conexion.conexion
     cursor = con.cursor(dictionary=True)
-    consulta = 'SELECT * FROM guia;'
+    consulta = 'SELECT * FROM guia_turistica;'
     cursor.execute(consulta)
     datos = cursor.fetchall()
     con.close()
---
+++ componentes/modelos.py
@@ -0,0 +1,40 @@
+from base_db.conexion_db import conexion
+from base_db.tabla_db import Tabla
+
+        
+class Guia_turistica(Tabla):
+    
+    tabla = 'guia_turistica'
+    conexion = conexion
+    campos = ('id', 'lugar_turistico_id', 'fecha_inicio', 'fecha_cierre', 'guia_id', 'cupo')
+    
+    def __init__(self, *args, de_bbdd=False):
+        super().crear(args, de_bbdd)
+        
+class Guia(Tabla):
+    
+    tabla = 'guia'
+    conexion = conexion
+    campos = ('id', 'nombre', 'dni')
+    
+    def __init__(self, *args, de_bbdd=False):
+        super().crear(args, de_bbdd)
+        
+class Lugar_turistico(Tabla):
+    
+    tabla = 'lugar_turistico'
+    conexion = conexion
+    campos = ('id', 'nombre', 'actividad_id', 'descripcion')
+    
+    def __init__(self, *args, de_bbdd=False):
+        super().crear(args, de_bbdd)
+        
+class Actividad(Tabla):
+    
+    tabla = 'actividad'
+    conexion = conexion
+    campos = ('id', 'actividad')
+    
+    def __init__(self, *args, de_bbdd=False):
+        super().crear(args, de_bbdd)
+
